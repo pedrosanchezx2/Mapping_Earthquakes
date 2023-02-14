ce02b4890aa9b98ce95bd67ceeeff400e6ed8546
pedrosanchezx2 <113708861+pedrosanchezx2@users.noreply.github.com>
2023-02-14 12:22:43 -0600

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,10 +1,17 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.243], 14);
+
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGVkcm9zYW5jaGV6eDIiLCJhIjoiY2xkejUwOTgzMHkwNDN4cDVoNWFhZm5yZiJ9.rRdMrmOGhDmjw4zl9TTG7g', {
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGVkcm9zYW5jaGV6eDIiLCJhIjoiY2xkejUwOTgzMHkwNDN4cDVoNWFhZm5yZiJ9.rRdMrmOGhDmjw4zl9TTG7g', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
